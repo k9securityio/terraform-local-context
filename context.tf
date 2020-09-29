f059a583d44773922d14872370f887e413f2a0fe
Stephen Kuenzli <skuenzli@k9security.io>
2020-09-29 08:27:31 -0700

    Upgrade to Terraform 0.12 & HCL2.

--- context.tf
+++ context.tf
@@ -1,92 +1,86 @@
 locals {
   standard_tags = {
-    Owner       = "${var.owner}"
-    Environment = "${var.env}"
-    Application = "${var.app}"
+    Owner       = var.owner
+    Environment = var.env
+    Application = var.app
     ManagedBy   = "Terraform"
   }
 
   opt_name = {
     exists = {
-      Name = "${var.name}"
+      Name = var.name
     }
-
     does_not_exist = {}
   }
 
   opt_role = {
     exists = {
-      Role = "${var.role}"
+      Role = var.role
     }
-
     does_not_exist = {}
   }
 
   opt_business_unit = {
     exists = {
-      BusinessUnit = "${var.business_unit}"
+      BusinessUnit = var.business_unit
     }
-
     does_not_exist = {}
   }
 
   opt_business_process = {
     exists = {
-      BusinessProcess = "${var.business_process}"
+      BusinessProcess = var.business_process
     }
-
     does_not_exist = {}
   }
 
   opt_cost_center = {
     exists = {
-      CostCenter = "${var.cost_center}"
+      CostCenter = var.cost_center
     }
-
     does_not_exist = {}
   }
 
   opt_compliance_scheme = {
     exists = {
-      ComplianceScheme = "${var.compliance_scheme}"
+      ComplianceScheme = var.compliance_scheme
     }
-
     does_not_exist = {}
   }
 
   opt_confidentiality = {
     exists = {
-      Confidentiality = "${var.confidentiality}"
+      Confidentiality = var.confidentiality
     }
-
     does_not_exist = {}
   }
 
   opt_integrity = {
     exists = {
-      Integrity = "${var.integrity}"
+      Integrity = var.integrity
     }
-
     does_not_exist = {}
   }
 
   opt_availability = {
     exists = {
-      Availability = "${var.availability}"
+      Availability = var.availability
     }
-
     does_not_exist = {}
   }
 
-  tags = "${merge(local.standard_tags
-  , local.opt_name[var.name != "" ? "exists" : "does_not_exist"]
-  , local.opt_role[var.role != "" ? "exists" : "does_not_exist"]
-  , local.opt_cost_center[var.cost_center != "" ? "exists" : "does_not_exist"]
-  , local.opt_business_unit[var.business_unit != "" ? "exists" : "does_not_exist"]
-  , local.opt_business_process[var.business_process != "" ? "exists" : "does_not_exist"]
-  , local.opt_compliance_scheme[var.compliance_scheme != "" ? "exists" : "does_not_exist"]
-  , local.opt_confidentiality[var.confidentiality != "" ? "exists" : "does_not_exist"]
-  , local.opt_integrity[var.integrity != "" ? "exists" : "does_not_exist"]
-  , local.opt_availability[var.availability != "" ? "exists" : "does_not_exist"]
-  , var.additional_tags)}"
+  tags = merge(
+    local.standard_tags,
+    local.opt_name[var.name != "" ? "exists" : "does_not_exist"],
+    local.opt_role[var.role != "" ? "exists" : "does_not_exist"],
+    local.opt_cost_center[var.cost_center != "" ? "exists" : "does_not_exist"],
+    local.opt_business_unit[var.business_unit != "" ? "exists" : "does_not_exist"],
+    local.opt_business_process[var.business_process != "" ? "exists" : "does_not_exist"],
+    local.opt_compliance_scheme[var.compliance_scheme != "" ? "exists" : "does_not_exist"],
+    local.opt_confidentiality[var.confidentiality != "" ? "exists" : "does_not_exist"],
+    local.opt_integrity[var.integrity != "" ? "exists" : "does_not_exist"],
+    local.opt_availability[var.availability != "" ? "exists" : "does_not_exist"],
+    var.additional_tags,
+  )
 }
+
--- test/fixtures/minimal/minimal.tf
+++ test/fixtures/minimal/minimal.tf
@@ -5,19 +5,19 @@ resource "random_id" "testing_suffix" {
 module "minimal_context" {
   source = "../../../"
 
-  owner = "${var.owner}"
-  env   = "${var.env}"
-  app   = "${var.app}"
+  owner = var.owner
+  env   = var.env
+  app   = var.app
 }
 
 module "full_context" {
   source = "../../../"
 
-  name = "${var.name}"
+  name = var.name
 
-  owner = "${var.owner}"
-  env   = "${var.env}"
-  app   = "${var.app}"
+  owner = var.owner
+  env   = var.env
+  app   = var.app
   role  = "blob store"
 
   business_unit    = "Enterprise Solutions"
@@ -36,37 +36,38 @@ module "full_context" {
 }
 
 variable "name" {
-  type = "string"
+  type = string
 }
 
 variable "region" {
-  type = "string"
+  type = string
 }
 
 variable "org" {
-  type = "string"
+  type = string
 }
 
 variable "owner" {
-  type = "string"
+  type = string
 }
 
 variable "env" {
-  type = "string"
+  type = string
 }
 
 variable "app" {
-  type = "string"
+  type = string
 }
 
 output "minimal_context-tags" {
-  value = "${module.minimal_context.tags}"
+  value = module.minimal_context.tags
 }
 
 output "full_context-tags" {
-  value = "${module.full_context.tags}"
+  value = module.full_context.tags
 }
 
 output "random_id-hex" {
-  value = "${random_id.testing_suffix.hex}"
+  value = random_id.testing_suffix.hex
 }
+
